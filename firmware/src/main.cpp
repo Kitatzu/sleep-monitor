d34ddb127f1977a69cb963aec7c5ea5bb1bfed28
Kitatzu <exequielmrodriguez@gmail.com>
2026-04-02 20:47:49 -0300

    SM-03 Add ldr logic for firmware

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,18 +1,43 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// --- Configuration ---
+#define PIN_LDR 1 
+const int LDR_FLOOR = 15;   
+const int LDR_CEIL  = 1500; 
+const int TICK_RATE = 2000; 
+
+unsigned long lastTick = 0;
+
+int readLightPercentage();
+void sendJson(int lightLevel);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    analogSetAttenuation(ADC_11db); // Standard 0-3.1V range
+    Serial.println("{\"status\": \"LDR_Module_Ready\"}");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    if (millis() - lastTick >= TICK_RATE) {
+        sendJson(readLightPercentage());
+        lastTick = millis();
+    }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+int readLightPercentage() {
+    int raw = analogRead(PIN_LDR);
+    // Linear mapping from raw ADC to percentage
+    int percentage = map(raw, LDR_FLOOR, LDR_CEIL, 0, 100);
+    return constrain(percentage, 0, 100);
+}
+
+void sendJson(int lightLevel) {
+    JsonDocument doc;
+    doc["sensor"] = "light_ldr";
+    doc["value"] = lightLevel;
+    doc["unit"] = "%";
+
+    serializeJson(doc, Serial);
+    Serial.println(); 
 }
